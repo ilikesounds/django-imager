a31081f67115289946db824fa31de91708f4e240
ilikesounds <email_id>
2016-09-14 17:12:37 -0700

    migrate attempt

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -20,7 +20,7 @@ class Photo(models.Model):
 
     def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+        return 'user_{0}/{1}'.format(instance.user.id, filename)
 
     photo_id = models.UUIDField(
                primary_key=True,
