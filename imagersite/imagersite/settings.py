bf63bed839425795850e0bfaea1b0335ebecd7e6
ilikesounds <email_id>
2016-09-21 16:08:10 -0700

    adding front-end css

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'imagersite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +68,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'imagersite.wsgi.application'
 
 
@@ -121,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'imagersite', 'static')
+
+]
 
 
 
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', ''),
+    url(r'^$', TemplateView.as_view(
+        template_name='imagersite/index.html')),
     url(r'^admin/', admin.site.urls),
 
     url(r'^images/', include('imager_images.urls')),
