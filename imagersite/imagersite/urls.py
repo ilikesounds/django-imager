eefb1b494eca8e60f250334f1e8d5afcd61f23df
ilikesounds <email_id>
2016-09-21 14:37:32 -0700

    twirling twirling twirling

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    #url(r'^images/', include('imager_images.urls'))
+    url(r'^profile/', include('imager_profile.urls'))
 ]
