69b8379ea69d21bec8c0a041690c1fbbcd406554
ilikesounds <jeffrey.n.torres@gmail.com>
2016-10-21 13:27:10 -0700

    generated requirements.pip

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_IMAGER_SK']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.161.83.67']
 
 
 # Application definition
