28b22f77fc28e4da3c77c451721887c2f3350318
ilikesounds <jeffrey.n.torres@gmail.com>
2016-10-21 16:23:32 -0700

    fixed requirements.pip

--- imagersite/settings.py
+++ imagersite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'foodlust', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'imagersite', 'static')]
 
 # Media file handling
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
